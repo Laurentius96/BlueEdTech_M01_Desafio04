ee0002c75742ddbed6fa90a01a588b6ca3fb5d63
Lorenzo C. Bianchi <bianchi92@hotmail.com>
2022-07-18 22:01:37 -0300

    Add introdução

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 console.clear();
-const prompt = require("prompt-sync")();
+const prompt = require('prompt-sync')();
+
+
+console.log(`\nIntrodução:\n`);
+console.log(`O príncipe Eric Roggandi está desaparecido. O ataque que o levou de sua comitiva não deixou sobreviventes - mas o modo de operação aponta para um infame criminoso que correga
+uma alcunha perversa. Agora, o soberano de Zakharov conta com você para levar Gazin Adagas à justiça e trazer para casa seu filho, o inestimável herdeiro do Reino das Armas. Um crime ousado 
+como este podo ter um objetivo simples... ou pode ser apenas o começo de uma trama assustadora e imprevisível.`)
+
