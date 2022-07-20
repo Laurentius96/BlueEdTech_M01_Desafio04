ce4e874172c4f3d5794349477d43c59431ea706f
Lorenzo C. Bianchi <bianchi92@hotmail.com>
2022-07-19 22:41:55 -0300

    Add texto na introdução

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
 console.clear();
 const prompt = require('prompt-sync')();
 
+console.log(`\nIntrodução:`);
+console.log(`
+O príncipe Eric Roggandi está desaparecido. O ataque que o levou de sua comitiva não deixou sobreviventes - mas o modo de operação aponta para um infame criminoso que correga
+uma alcunha perversa. Agora, o soberano de Zakharov conta com você para levar Gazin Adagas à justiça e trazer para casa seu filho, o inestimável herdeiro do Reino das Armas. 
+Um crime ousado como este podo ter um objetivo simples... ou pode ser apenas o começo de uma trama assustadora e imprevisível?
 
-console.log(`\nIntrodução:\n`);
-console.log(`O príncipe Eric Roggandi está desaparecido. O ataque que o levou de sua comitiva não deixou sobreviventes - mas o modo de operação aponta para um infame criminoso que correga
-uma alcunha perversa. Agora, o soberano de Zakharov conta com você para levar Gazin Adagas à justiça e trazer para casa seu filho, o inestimável herdeiro do Reino das Armas. Um crime ousado 
-como este podo ter um objetivo simples... ou pode ser apenas o começo de uma trama assustadora e imprevisível.`)
+O Rei Walfengarr Roggandin espera por uma solução, hospedado em um forte próximo, acompanhando de perto as investigações ainda ignoradas pela população. Sem saber Gazin Adagas 
+espera por você. E você espera por respostas. 
 
+Você entra na Taverna do Dragão Morto... e aqui que sua aventura começa. `);
+
+console.log(`
+
+`);
