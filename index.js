b28f48ef5da6a9ad98d0f7098f9e0dd91ff35a5e
Lorenzo C. Bianchi <bianchi92@hotmail.com>
2022-07-21 16:55:09 -0300

    Add referência 09

--- index.js
+++ index.js
@@ -93,3 +93,26 @@ comunicá-lo que você irá morrer. Então ele ataca Ele bate no peito em desafi
 =============================================================================================================================================================================
 
 `);
+
+
+console.log(`
+
+=== 9 === Veio de (7) - Teste de força
+
+Agora é a vez de Gazin o atacar! Ele corre em sua direção com uma adaga levantada acima da cabeça e salta sobre você! Mal tendo se recobrado do combate anterior, tudo que pode
+fazer é jogar-se para trás com o pé no estômago do atacante e arremessá-lo contra a parede do fundo. Ele derruba a adaga, mas logo está novamente sobre você.
+
+Ambos rolam pelo chão derrubando mesas e cadeiras. Ele enfia os dedos em seu rosto, tentando atingir seus olhos. Você afasta a mão de Gazin com uma mordida e esmurra o 
+rosto dele repetidas vezes, até que ele quebra um caneco de madeira atrás de sua cabeça. Você desaba e Gazin aproveita a oportunidade para engatinhar até a adaga.
+
+Enquanto isso, em sua tontura, você pode ver os goblins que frequentavam o Dragão Morto surrupiar os pertences alheios daqueles vidrados na briga, até que um grito o desperta
+da letargia. Gazin Adagas cai sobre você, pronto para furar-lhe com sua arma. Você bloqueia o golpe por pura sorte, desarmando novamente o adversário, mas colocando-se em uma
+posição de tremenda desvantagem.
+
+>> Teste sua força:
+=============================================================================================================================================================================
+(10) Sucesso!
+(11) Fracasso!
+=============================================================================================================================================================================
+
+`);
