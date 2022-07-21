ed5184075ed08dd7497bff8e7b3f3963d464687f
Lorenzo C. Bianchi <bianchi92@hotmail.com>
2022-07-20 21:36:08 -0300

    Add 1° referência e correção ortográfica

--- index.js
+++ index.js
@@ -3,15 +3,34 @@ const prompt = require('prompt-sync')();
 
 console.log(`\nIntrodução:`);
 console.log(`
-O príncipe Eric Roggandi está desaparecido. O ataque que o levou de sua comitiva não deixou sobreviventes - mas o modo de operação aponta para um infame criminoso que correga
+O príncipe Eric Roggandi está desaparecido. O ataque que o levou de sua comitiva não deixou sobreviventes - mas o modo de operação aponta para um infame criminoso que carrega
 uma alcunha perversa. Agora, o soberano de Zakharov conta com você para levar Gazin Adagas à justiça e trazer para casa seu filho, o inestimável herdeiro do Reino das Armas. 
 Um crime ousado como este podo ter um objetivo simples... ou pode ser apenas o começo de uma trama assustadora e imprevisível?
 
 O Rei Walfengarr Roggandin espera por uma solução, hospedado em um forte próximo, acompanhando de perto as investigações ainda ignoradas pela população. Sem saber Gazin Adagas 
 espera por você. E você espera por respostas. 
 
-Você entra na Taverna do Dragão Morto... e aqui que sua aventura começa. `);
+Você entra na Taverna do Dragão Morto, onde o principal suspeito está... e aqui que sua aventura começa! 
+`);
 
 console.log(`
+
+=== 1 ===
+
+Ao entrar na taverna, acaba por virar o centro das atenções, o novo forasteriro. Seu chapelçao de couro esconde seus traços e identidade, mas todos parecem indiferentes com
+sua presença. Olhando o ambiente Gazin Adagas não parece tê-lo reconhecido, assim como os demais, como oficial do Rei Walfengarr.
+
+Ao olhar melhor para seu alvo, você nota que ele é um homem baixo, com porte largo, mas pouco atlético com uma aparência descuidada e vestimentas rasgadas. Entretando, não
+deixe isso dar motivo para subestimar sua periculosidade.
+
+Examinado a taverna, você nota co local é espaçoso e com andares superiores são integrados ao salão principal. No andar de cima, as portos dos quartos de pernoite podem ser
+vistas podem ser vistas além de um mezanino, protegida por um parapeito. O tarveneiro é um homem de meia-idade, ruivo e barrigudo.
+
+>> O que você fará? - digite o número para escolher:
+=============================================================================================================================================================================
+(2) Vá anunciar que veio para levar Gazin Adagas à justiça?
+(3) Vai entra quieto, escolher um lugar e pedir algo, esprando uma oportunidade menos tumultuosa de cumprir seu dever?
+(4) Vai se aproximar de algum indivíduo do salão comunal e puxar conversa?
+=============================================================================================================================================================================
 
 `);
